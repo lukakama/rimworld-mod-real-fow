2dbd42a986dc83ab76e98d7ad7ed259c1d043fec
Luca De Petrillo <lukakama@gmail.com>
2017-02-19 20:02:22 +0100

    Doors now correctly update the fog of war when opened and closed.

--- Sources/RimWorldRealFoW/ThingComps/CompViewBlockerWatcher.cs
+++ Sources/RimWorldRealFoW/ThingComps/CompViewBlockerWatcher.cs
@@ -9,7 +9,7 @@ namespace RimWorldRealFoW.ThingComps {
 		private Map map;
 		private MapComponentSeenFog mapCompSeenFog;
 
-		private bool viewBloker;
+		private bool lastCanBeSeenOver;
 		private Building b;
 
 		public override void PostSpawnSetup() {
@@ -17,26 +17,48 @@ namespace RimWorldRealFoW.ThingComps {
 
 			b = parent as Building;
 
-			if (b != null && !b.CanBeSeenOver()) {
-				viewBloker = true;
+			if (b != null) {
+				// Default as see-through.
+				lastCanBeSeenOver = true;
 
-				map = parent.Map;
-				mapCompSeenFog = map.getMapComponentSeenFog();
+				updateViewBlocker();
+			}
+		}
 
+		public override void ReceiveCompSignal(string signal) {
+			base.ReceiveCompSignal(signal);
+
+			if (b != null) {
+				updateViewBlocker();
+			}
+		}
+
+		public override void CompTick() {
+			base.CompTick();
+
+			if (b != null) {
+				updateViewBlocker();
+			}
+		}
+
+		public override void PostDeSpawn(Map map) {
+			base.PostDeSpawn(map);
+
+			// When de-spawn, if the thing was blocking the view, then trigger a FoV update for near objects.
+			if (b != null && !lastCanBeSeenOver) {
 				bool[] viewBlockerCells = mapCompSeenFog.viewBlockerCells;
-				
+
 				int mapSizeZ = map.Size.z;
 				CellRect occupiedRect = b.OccupiedRect();
 				for (int x = occupiedRect.minX; x <= occupiedRect.maxX; x++) {
 					for (int z = occupiedRect.minZ; z <= occupiedRect.maxZ; z++) {
-						viewBlockerCells[(z * mapSizeZ) + x] = true;
+						viewBlockerCells[(z * mapSizeZ) + x] = false;
 					}
 				}
 
 				if (Current.ProgramState == ProgramState.Playing) {
-					// When spawn, if the thing si blocking the view, then trigger a FoV update for everything on the map (if any).
 					if (parent.Map != null) {
-						List<Thing> things = parent.Map.listerThings.AllThings;
+						List<Thing> things = map.listerThings.AllThings;
 						for (int i = 0; i < things.Count; i++) {
 							ThingWithComps thing = things[i] as ThingWithComps;
 							if (thing != null) {
@@ -51,28 +73,33 @@ namespace RimWorldRealFoW.ThingComps {
 			}
 		}
 
-		public override void PostDeSpawn(Map map) {
-			base.PostDeSpawn(map);
-			if (viewBloker) {
+		private void updateViewBlocker() {
+			if (lastCanBeSeenOver != b.CanBeSeenOver()) {
+				lastCanBeSeenOver = b.CanBeSeenOver();
+
+				map = parent.Map;
+				mapCompSeenFog = map.getMapComponentSeenFog();
+
 				bool[] viewBlockerCells = mapCompSeenFog.viewBlockerCells;
 
 				int mapSizeZ = map.Size.z;
 				CellRect occupiedRect = b.OccupiedRect();
 				for (int x = occupiedRect.minX; x <= occupiedRect.maxX; x++) {
 					for (int z = occupiedRect.minZ; z <= occupiedRect.maxZ; z++) {
-						viewBlockerCells[(z * mapSizeZ) + x] = false;
+						viewBlockerCells[(z * mapSizeZ) + x] = !b.CanBeSeenOver();
 					}
 				}
 
 				if (Current.ProgramState == ProgramState.Playing) {
-					// When de-spawn, if the thing was blocking the view, then trigger a FoV update for everything on the map.
-					List<Thing> things = map.listerThings.AllThings;
-					for (int i = 0; i < things.Count; i++) {
-						ThingWithComps thing = things[i] as ThingWithComps;
-						if (thing != null) {
-							CompFieldOfViewWatcher cmpFov = thing.GetComp<CompFieldOfViewWatcher>();
-							if (cmpFov != null && b.Position.InHorDistOf(thing.Position, cmpFov.sightRange)) {
-								cmpFov.updateFoV(true);
+					if (parent.Map != null) {
+						List<Thing> things = parent.Map.listerThings.AllThings;
+						for (int i = 0; i < things.Count; i++) {
+							ThingWithComps thing = things[i] as ThingWithComps;
+							if (thing != null) {
+								CompFieldOfViewWatcher cmpFov = thing.GetComp<CompFieldOfViewWatcher>();
+								if (cmpFov != null && b.Position.InHorDistOf(thing.Position, cmpFov.sightRange)) {
+									cmpFov.updateFoV(true);
+								}
 							}
 						}
 					}
