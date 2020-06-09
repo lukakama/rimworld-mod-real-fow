46ad88ccc3429f2c279303796475545ea2dc1530
Rando Wiltschek <randomw@gmx.net>
2020-06-09 12:46:34 +0200

    - Changes spaces to tabs

--- Sources/RimWorldRealFoW/Detours/_GenView.cs
+++ Sources/RimWorldRealFoW/Detours/_GenView.cs
@@ -1,24 +1,23 @@
-﻿using System.Collections.Generic;
-using RimWorld;
+﻿using RimWorld;
 using Verse;
 
 namespace RimWorldRealFoW.Detours {
-    public static class _GenView {
+	public static class _GenView {
+		private static MapComponentSeenFog lastUsedMapComponent;
+		private static Map lastUsedMap;
 
-        private static MapComponentSeenFog lastUsedMapComponent;
-        private static Map lastUsedMap;
+		public static void ShouldSpawnMotesAt_Postfix(IntVec3 loc, Map map, ref bool __result)
+		{
+			if (!__result) return;
+			// Cache map component for fast repeated retrieval
+			var comp = lastUsedMapComponent;
+			if (map != lastUsedMap)
+			{
+				lastUsedMap = map;
+				comp = lastUsedMapComponent = map.GetComponent<MapComponentSeenFog>();
+			}
 
-        public static void ShouldSpawnMotesAt_Postfix(IntVec3 loc, Map map, ref bool __result)
-        {
-            if (!__result) return;
-            // Cache map component for fast repeated retrieval
-            var comp = lastUsedMapComponent;
-            if (map != lastUsedMap)
-            {
-                lastUsedMap = map;
-                comp = lastUsedMapComponent = map.GetComponent<MapComponentSeenFog>();
-            }
-            __result = comp == null || comp.isShown(Faction.OfPlayer, loc.x, loc.z);
-        }
-    }
+			__result = comp == null || comp.isShown(Faction.OfPlayer, loc.x, loc.z);
+		}
+	}
 }
